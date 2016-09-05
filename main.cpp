32ef1555d35beed1855ca17cb7b8723451b2d630
Benson Chu <bensonchu457@gmail.com>
2016-09-04 20:18:26 -0500

    Not displaying comments

--- main.cpp
+++ main.cpp
@@ -38,52 +38,25 @@ tuple<int,char> getInfo(string s)
 
 
 int main (int argc, const char * argv[]) {
+	
+    int minutes = 0;
+    int hours = 0;
+    int days = 0;
+    
+    
 	string line;
-	string s = "";
 	ifstream myfile (argv[1]);
 	if (myfile.is_open())
 	{
 		while ( getline (myfile,line) )
 		{
-			s = s + line + "\n";
+			if(line.at(0) != '#')
+			{
+				cout << line << endl;
+			}
 		}
 		myfile.close();
 	}
- 
-	std::regex no_comments("#.*?\n");
-    auto lines_begin = 
-        std::sregex_iterator(s.begin(), s.end(), no_comments);
-    auto lines_end = std::sregex_iterator();
-    
-    string remove_comments = regex_replace(s, no_comments, "");
- 
-    std::regex word_regex("([0-9]*)\\ ([Mm]inute(s|\\ )|[Hh]our(s|\\ )|[Dd]ay(s|\\ ))");
-    auto words_begin = 
-        std::sregex_iterator(remove_comments.begin(), remove_comments.end(), word_regex);
-    auto words_end = std::sregex_iterator();
-    int minutes = 0;
-    int hours = 0;
-    int days = 0;
-    
-    int quantity;
-    char value;
-    
-    for (std::sregex_iterator i = words_begin; i != words_end; ++i) {
-        std::smatch match = *i;
-        std::string match_str = match.str();
-        std::tie (quantity, value) = getInfo(match_str);
-        
-        switch(value)
-        {
-			case 'a': days += quantity;
-						break;
-			case 'i': minutes += quantity;
-						break;
-			case 'o': hours += quantity;
-						break;
-		}
-    }
-		
     
     cout << "Minutes: " << minutes << endl;
     cout << "Hours: " << hours << endl;
